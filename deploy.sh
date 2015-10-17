af959c9132d57f334321ab68db7de814b12d9eeb
Mohammad Rafiee <m.rafieee@gmail.com>
2015-10-16 21:41:54 -0400

    dictionary backup moved to roo for some checks

--- deploy.sh
+++ deploy.sh
@@ -4,12 +4,18 @@ echo "$(date) -- automatic deployment from jenkins started." >> $LOGFILE
 find ./ -name '*.pyc'
 echo "$(date) -- pyc files has benn removed from built project." >> $LOGFILE
 find ./ -name '*.pyc' -delete
+
 FILENAME='/var/www/anagrams_$( date '+%Y-%m-%d_%H-%M-%S' ).tar.gz'
+
 tar -czf $FILENAME /var/www/anagrams 1>&2 >> $LOGFILE
 echo '$(date) -- backup file $(FILENAME) created.' >> $LOGFILE
 shopt -s extglob
-rm /var/www/anagrams/* !(db.sqlite3|dictionary.txt) 1>&2 >> $LOGFILE
-echo '$(date) -- /var/www/anagrams/ folder has been cleared.' >> $LOGFILE
-cp !(db.sqlite3|dictionary.txt) /var/www/anagrams/
-shopt -u extglob
 
+cd /var/www/anagrams
+
+rm -r !(db.sqlite3|dictionary.txt) 1>&2 >> $LOGFILE
+echo '$(date) -- /var/www/anagrams/ folder has been cleared.' >> $LOGFILE
+cd /root/anagrams_last
+
+cp -r !(db.sqlite3|dictionary.txt) /var/www/anagrams/
+shopt -u extglob
