a643d0e08c0b4c80e270d5b7c87f9d15bc08d54d
Mohammad Rafiee <m.rafieee@gmail.com>
2015-10-16 20:50:22 -0400

    depploy.sh and log file added

---
+++ deploy.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+LOGFILE='logs/deployment.log'
+echo "$(date) -- automatic deployment from jenkins started." >> $LOGFILE
+find ./ -name '*.pyc'
+echo "$(date) -- pyc files has benn removed from built project." >> $LOGFILE
+find ./ -name '*.pyc' -delete
+FILENAME='/var/www/anagrams_$( date '+%Y-%m-%d_%H-%M-%S' ).tar.gz'
+tar -czf $FILENAME /var/www/anagrams 1>&2 >> $LOGFILE
+echo '$(date) -- backup file $(FILENAME) created.' >> $LOGFILE
+shopt -s extglob
+rm /var/www/anagrams/* !(db.sqlite3|dictionary.txt) 1>&2 >> $LOGFILE
+echo '$(date) -- /var/www/anagrams/ folder has been cleared.' >> $LOGFILE
+cp !(db.sqlite3|dictionary.txt) /var/www/anagrams/
+shopt -u extglob
+
