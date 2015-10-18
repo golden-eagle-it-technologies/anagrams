9ca192970e8aef4cd0902c8fca3b00f32d07e9f0
Mohammad Rafiee <m.rafieee@gmail.com>
2015-10-18 19:33:13 -0400

    some admin changes and a file rename

--- apps/anagrams/admin.py
+++ apps/anagrams/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
-from models import WordAnagrams
+from models import WordAnagrams,Suggestions
+
 
 admin.site.register(WordAnagrams)
+admin.site.register(Suggestions)
