fcf15c74d5cd43d2d621362e9f70e25ed9485bbb
Mohammad Rafiee <m.rafieee@gmail.com>
2015-10-18 23:35:54 -0400

    apps test on hook

--- anagrams/settings.py
+++ anagrams/settings.py
@@ -75,7 +75,7 @@ WSGI_APPLICATION = 'anagrams.wsgi.application'
 
 # Jenkins app
 JENKINS_TASKS = (
-#    'django_jenkins.tasks.with_coverage',
+    'django_jenkins.tasks.with_coverage',
     'django_jenkins.tasks.run_pep8',
     'django_jenkins.tasks.run_pyflakes',
     'django_jenkins.tasks.run_sloccount'
